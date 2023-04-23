4d04f3088ff71963ca1216426c335d2f85c5d61d
Mourid Chaimaa <chaimaa.mourid1@gmail.com>
2023-04-23 13:32:32 -0700

    4-puts.c
    	modified:   4-puts.c
    
    	new file:   4-puts.c
    	modified:   README.md
    	modified:   4-puts.c
    
    	new file:   4-puts.c
    	modified:   README.md
    	modified:   4-puts.c
    	../README.md
    
    	deleted:    4-puts.c
    
    	new file:   4-puts.c
    	modified:   README.md
    
    	new file:   4-puts.c
    
    	new file:   4-puts.c
    	modified:   README.md
    	modified:   4-puts.c
    
    	new file:   4-puts.c
    
    	new file:   4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* main - print a line with a puts function 
+ * return - ok  
+ * */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
