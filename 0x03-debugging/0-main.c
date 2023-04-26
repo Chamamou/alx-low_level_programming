2ef4e465da966d6d3ee19d211a992cf6094cbbca
Mourid Chaimaa <chaimaa.mourid1@gmail.com>
2023-04-26 16:58:31 -0700

    modified:   0-main.c 	modified:   main.h
    	modified:   0-main.c
    	modified:   main.h
    
    	modified:   0-main.c
    	modified:   main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,9 +3,12 @@
  * main - Testing the function if it is positive or negative
  * Return: 0
  */
+
 int main(void)
 {
+	void positive_or_negative(int i);
 	int i;
+
 	i = 0;
 	positive_or_negative(i);
 	return (0);
